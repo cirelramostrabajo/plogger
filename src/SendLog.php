8e16d7106be28a9a6593aae410ed399a11cc50e4
Cirel Ramos <cirelramos@gmail.com>
2020-08-14 23:30:05 -0500

    first commit

---
+++ src/SendLog.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Cirelramostrabajo\Plogger;
+
+class SendLog
+{
+    public function __construct()
+    {
+        $code = '';
+    }
+
+    public function greet($greet = "Hello World")
+    {
+        return $greet;
+    }
+}
+
+
+class Index
+{
+}
