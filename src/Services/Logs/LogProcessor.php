ef21facc0224b56e7c88f257acc37427a1fb0540
Cirel Ramos <cirelramos@gmail.com>
2020-08-16 11:35:37 -0500

    add new Log sendUdpLogJson and async send tcp

---
+++ src/Services/Logs/LogProcessor.php
@@ -0,0 +1,16 @@
+<?php
+namespace Cirelramostrabajo\Plogger\Services\Logs;
+
+class LogProcessor
+{
+    public function __invoke(array $record)
+    {
+        $record['extra'] = [
+            'user_id' => auth()->user() ? auth()->user()->id : NULL,
+            'origin' => request()->headers->get('origin'),
+            'ip' => request()->server('REMOTE_ADDR'),
+            'user_agent' => request()->server('HTTP_USER_AGENT')
+        ];
+        return $record;
+    }
+}
